122a49930e9c7f877c75dd6fb989ce51a7ca5e60
JasonW <silentred3@gmail.com>
2017-08-10 14:37:09 +0800

    add comments

--- myStrace/strace.go
+++ myStrace/strace.go
@@ -38,16 +38,23 @@ func main() {
 			if err != nil {
 				break
 			}
+			//fmt.Printf("%#v \n",regs)
 			name := ss.getName(regs.Orig_rax)
 			fmt.Printf("name: %s, id: %d \n", name, regs.Orig_rax)
 			ss.inc(regs.Orig_rax)
 		}
 
+		/**
+		http://www.linuxjournal.com/article/6100?page=0,1
+		Here we are tracing the write system calls, and ls makes three write system calls. The call to ptrace, with a first argument of PTRACE_SYSCALL, makes the kernel stop the child process whenever a system call entry or exit is made. It's equivalent to doing a PTRACE_CONT and stopping at the next system call entry/exit.
+		*/
 		err = syscall.PtraceSyscall(pid, 0)
 		if err != nil {
 			panic(err)
 		}
 
+		// http://www.linuxjournal.com/article/6100?page=0,1
+		//The status variable in the wait call is used to check whether the child has exited. This is the typical way to check whether the child has been stopped by ptrace or was able to exit.
 		_, err = syscall.Wait4(pid, nil, 0, nil)
 		if err != nil {
 			panic(err)
